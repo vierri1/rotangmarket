887c886de7cc91b43c13404342bdae42857c5917
Andrey <avpoponin@yandex.ru>
2020-05-30 13:26:32 +0300

    added get products

--- src/main/java/com/market/rotang/rotangmarket/controller/MainController.java
+++ src/main/java/com/market/rotang/rotangmarket/controller/MainController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
@@ -39,8 +40,9 @@ public class MainController {
         return "checkout";
     }
 
-    @GetMapping("/product-details")
-    public String productDetails() {
+    @GetMapping("/product-details/{id}")
+    public String productDetails(@PathVariable Long id,
+                                 Model model) {
         return "product-details";
     }
 
--- src/main/java/com/market/rotang/rotangmarket/entity/Product.java
+++ src/main/java/com/market/rotang/rotangmarket/entity/Product.java
@@ -4,6 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.util.CollectionUtils;
 
 import javax.persistence.*;
 import java.math.BigDecimal;
@@ -18,15 +19,27 @@ public class Product {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.EAGER)
     @JoinColumn(name = "category_id", nullable = false)
     private Category category;
     @Column(unique = true)
     private String name;
     private String description;
     private BigDecimal price;
-    @OneToMany(mappedBy = "productId")
+    @OneToMany(mappedBy = "productId", fetch = FetchType.EAGER)
     private List<ProductImage> images;
+
+    public ProductImage getMainImage() {
+        return CollectionUtils.isEmpty(images)
+                ? null
+                : images.get(0);
+    }
+
+    public ProductImage getAltImage() {
+        return CollectionUtils.isEmpty(images) || images.size() < 2
+                ? null
+                : images.get(1);
+    }
 }
 
 
